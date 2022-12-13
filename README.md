b6669dcac105e03cc651a71bab4e60a45779a5ff
Bingchen Ji <jjjbbbccc1257@gmail.com>
2022-12-13 16:37:15 -0500

    minor edit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # JobReccer
 A job recommendation system for candidates
+Cold start (without data): 
+In project root directory, run $python ./scripts/rating.py
+
+Normal start:
+Start with non-empty job_user_rating.csv:
+In project root directory, run $python run.py
+
---
+++ producequery.py
@@ -0,0 +1,26 @@
+import re
+
+with open('./resume/resume_output/SDE2.txt') as f:
+    lines = f.readlines()
+year = list()
+query = dict()
+for line in lines:
+    degree1 = re.search('^Bachelor', line)
+    degree2 = re.search('^Master', line)
+    degree3 = re.search('^Phd', line)
+    uni = re.search('^University', line)
+    if (uni != None):
+        year = [int(s) for s in re.findall(r'\b\d+\b', line)]
+    if (degree1 != None):
+        query["degree"] = line
+    if (degree2 != None):
+        query["degree"] = line
+    if (degree3 != None):
+        query["degree"] = line
+
+    language = re.search('^Language', line)
+    if (language != None):
+        query["technique"] = line
+query["year"] = year[0]
+
+print(query)
